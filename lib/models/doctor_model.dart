feb1daca2ec1f9f32d75b83b403431cfa343a43b
Fauzan <ojanaja03@gmail.com>
2024-01-18 18:34:10 +0700

    first commit

---
+++ lib/models/doctor_model.dart
@@ -0,0 +1,27 @@
+import 'package:slicing_ui_flutter/models/service_model.dart';
+
+class DoctorModel {
+  String name;
+  String image;
+  List<String> services;
+  int distance;
+
+  DoctorModel(
+      {required this.name,
+      required this.image,
+      required this.services,
+      required this.distance});
+}
+
+var doctors = [
+  DoctorModel(
+      name: "Dr. Stone ",
+      image: "Dr. Stone.jpg",
+      services: [Service.vaccine, Service.surgery],
+      distance: 10),
+  DoctorModel(
+      name: "Dr. Stone ",
+      image: "Dr. Stone.jpg",
+      services: [Service.vaccine, Service.surgery],
+      distance: 10),
+];
